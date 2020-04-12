3c26032c43e843c184c401083cab90785edfd868
Matthew <matthew.poyner79@gmail.com>
2020-04-12 16:21:58 +1200

    made a test change to app.py to check that templating works when deployed to Heroku

--- app.py
+++ app.py
@@ -4,15 +4,21 @@ from os import path
 if path.exists("env.py"):
     import env
 
-from flask import Flask, render_template, redirect, request, url_for, request
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+app.config["MONGO_DBNAME"] = 'cooking'
+app.config["MONGO_URI"] = os.environ["MONGO_URI"]
+
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"), 
