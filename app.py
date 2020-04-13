652d392015724d7c1f74a055dfd2a3fdd2b50578
Matthew <matthew.poyner79@gmail.com>
2020-04-13 22:12:56 +1200

    added functionality to add recipe template

--- app.py
+++ app.py
@@ -18,14 +18,36 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    a_recipe = mongo.db.recipes.find()
-    return render_template("recipes.html", recipes=mongo.db.recipes.find())
+    return render_template("index.html", recipes=mongo.db.recipes.find())
+
+@app.route('/enter_recipe')
+def enter_recipe():
+    recipes=mongo.db.recipes.find()
+    cuisine=mongo.db.cuisine.find().sort('cuisine_name',1)
+    main_ingredient=mongo.db.main_ingredient.find().sort('main_ingredient',1)
+    return render_template('enter_recipe.html', recipes=recipes, cuisine=cuisine, main_ingredient=main_ingredient)
+
+@app.route('/show_recipes')
+def show_recipes():
+    recipes=mongo.db.recipes.find().sort('recipe_name', 1)
+    return render_template("show_recipes.html", recipes=recipes)
 
 
-
-@app.route('/get_recipes')
-def get_recipes():
-    return render_template("recipes.html", recipes=mongo.db.recipes.find())
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one({
+        'recipe_name':request.form['recipe_name'],
+        'cook_name':request.form['cook_name'],
+        'cuisine_name':request.form['cuisine_name'],
+        'main_ingredient':request.form['main_ingredient'],
+        'prep_time':request.form['prep_time'],
+        'total_cooking_time':request.form['total_cooking_time'],
+        'fat_per_serve':request.form['fat_per_serve'],
+        'ingredients':request.form['ingredients'],
+        'instructions':request.form['instructions'],
+    })
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"), 
