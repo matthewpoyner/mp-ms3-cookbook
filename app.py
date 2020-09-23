12d13d73e956ef8d3abca3faf907d11e68985c15
Matthew <matthew.poyner79@gmail.com>
2020-09-24 09:59:26 +1200

    refactored code within app.py, set a default image in recipe cards if no image provided by the user, added search field to index.html - to be wired up

--- app.py
+++ app.py
@@ -5,16 +5,17 @@ if path.exists("env.py"):
     import env
 
 from flask import (Flask, render_template, redirect,
-request, url_for, flash, session)
+                   request, url_for, flash, session)
 import bcrypt
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+import re
 
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'cooking'
-app.config['MONGO_URI'] = os.environ.get('MONGO_URI','mongodb://localhost')
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI', 'mongodb://localhost')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 mongo = PyMongo(app)
@@ -30,6 +31,7 @@ def index():
 def site_admin():
     return render_template('site_admin.html',  title="Site Admin")
 
+
 @app.route('/logout')
 def logout():
     session.clear()
@@ -40,18 +42,18 @@ def logout():
 def login():
     if request.method == 'POST':
         users = mongo.db.users
-        login_user = users.find_one({'username' : request.form['username']})
+        login_user = users.find_one({'username': request.form['username']})
 
         if not login_user:
             flash('Incorrect username', "warning")
 
         if login_user:
-            if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
+            if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'])==login_user['password']:
                 session['username'] = request.form['username']
                 flash(session['username'] +' you have logged in successfully!', 'success')
                 return render_template("index.html", username=session['username'],
                         recipes=mongo.db.recipes.find(),courses=mongo.db.meal_courses.find(), title='Welcome '+session['username'])
-            flash('Incorrect password', 'danger')  
+            flash('Incorrect password', 'danger')
 
     return render_template('login.html', title='Login')
 
@@ -74,18 +76,25 @@ def register():
 
     return render_template('register.html', title='Sign Up!')
 
+
 ''' end of code from Anthony Herbert -> Pretty Printed '''
+
+
 ''' Recipe section '''
 @app.route('/enter_recipe')
 def enter_recipe():
     if 'username' in session:
-        recipes=mongo.db.recipes.find()
-        cuisines=mongo.db.cuisines.find().sort('cuisine_name',1)
-        meal_course=mongo.db.meal_course.find().sort('course_name',1)
-        main_ingredient=mongo.db.main_ingredient.find().sort('main_ingredient',1)
-        return render_template('enter_recipe.html', title='Enter a new recipe', recipes=recipes, 
-                                cuisines=cuisines, main_ingredient=main_ingredient, 
-                                meal_course=meal_course, cook_name=session['username'])
+        recipes = mongo.db.recipes.find()
+        cuisines = mongo.db.cuisines.find().collation({'locale': 'en'}).sort('cuisine_name', 1)
+        meal_course = mongo.db.meal_course.find().sort('course_name', 1)
+        main_ingredient = mongo.db.main_ingredient.find().sort(
+                                                        'main_ingredient', 1)
+        return render_template('enter_recipe.html', title='Enter a new recipe',
+                               recipes=recipes,
+                               cuisines=cuisines,
+                               main_ingredient=main_ingredient,
+                               meal_course=meal_course,
+                               cook_name=session['username'])
     else:
         flash('You must be logged in to add a recipe', 'warning')
         return render_template('login.html', title='Login to add recipe')
@@ -103,16 +112,16 @@ def insert_recipe():
         'prep_time': req.get('prep_time'),
         'total_cooking_time': req.get('total_cooking_time'),
         'servings': req.get('servings'),
-        'ingredients':req.get('ingredients').splitlines(),
-        'instructions':req.get('instructions').splitlines(),
-        'image_src':req.get('image_src'),
-        'meal_course':req.get('meal_course'),
-        'protein':req.get('protein'),
-        'fat_per_serve':req.get('fat_per_serve'),
-        'carbohydrate':req.get('carbohydrate'),
-        'dietary_fibre':req.get('dietary_fibre'),
-        'cholesterol':req.get('cholesterol'),
-        'energy':req.get('energy'),
+        'ingredients': req.get('ingredients').splitlines(),
+        'instructions': req.get('instructions').splitlines(),
+        'image_src': req.get('image_src'),
+        'meal_course': req.get('meal_course'),
+        'protein': req.get('protein'),
+        'fat_per_serve': req.get('fat_per_serve'),
+        'carbohydrate': req.get('carbohydrate'),
+        'dietary_fibre': req.get('dietary_fibre'),
+        'cholesterol': req.get('cholesterol'),
+        'energy': req.get('energy'),
         })
     recipe = req.get('recipe_name')
     flash(f'Your recipe for {recipe} has been successfully added!', 'success')
@@ -121,15 +130,18 @@ def insert_recipe():
 
 @app.route('/show_one_recipe/<recipe_id>')
 def show_one_recipe(recipe_id):
-    the_recipe=mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
-    return render_template("show_one_recipe.html", recipes=the_recipe)
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    cook_name = mongo.db.recipes['cook_name']
+    return render_template("show_one_recipe.html", recipes=the_recipe, cook_name=cook_name)
 
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
-    cuisines = mongo.db.cuisines.find().sort('cuisine_name',1)
+    cuisines = mongo.db.cuisines.find().collation({'locale': 'en'}).sort('cuisine_name', 1)
     meal_course = mongo.db.meal_course.find().sort('course_name',1)
-    main_ingredient = mongo.db.main_ingredient.find().sort('main_ingredient',1)
-    return render_template("edit_recipe.html", cuisines=cuisines, meal_course = meal_course, main_ingredient=main_ingredient,
+    main_ingredient = mongo.db.main_ingredient.find().sort('main_ingredient', 1)
+    return render_template("edit_recipe.html", cuisines=cuisines,
+                           meal_course=meal_course,
+                           main_ingredient=main_ingredient,
     recipe = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)}))
 
 
@@ -139,32 +151,33 @@ def update_recipe(recipe_id):
     req = request.form
     recipes.update_one({'_id': ObjectId(recipe_id)},
     {
-        '$set':{
-    'recipe_name': req.get('recipe_name'),
-    'cook_name': req.get('cook_name'),
-    'cuisine_name':req.get('cuisine_name'),
-    'main_ingredient': req.get('main_ingredient'),
-    'prep_time': req.get('prep_time'),
-    'total_cooking_time':req.get('total_cooking_time'),
-    'servings': req.get('servings'),
-    'ingredients':req.get('ingredients').splitlines(),
-    'instructions':req.get('instructions').splitlines(),
-    'image_src':req.get('image_src'),
-    'meal_course':req.get('meal_course'),
-    'protein':req.get('protein'),
-    'fat_per_serve':req.get('fat_per_serve'),
-    'carbohydrate':req.get('carbohydrate'),
-    'dietary_fibre':req.get('dietary_fibre'),
-    'cholesterol':req.get('cholesterol'),
-    'energy':req.get('energy')
-    }})
+        '$set': {
+        'recipe_name': req.get('recipe_name'),
+        'cook_name': session['username'],
+        'cuisine_name': req.get('cuisine_name'),
+        'main_ingredient': req.get('main_ingredient'),
+        'prep_time': req.get('prep_time'),
+        'total_cooking_time': req.get('total_cooking_time'),
+        'servings': req.get('servings'),
+        'ingredients': req.get('ingredients').splitlines(),
+        'instructions': req.get('instructions').splitlines(),
+        'image_src': req.get('image_src'),
+        'meal_course': req.get('meal_course'),
+        'protein': req.get('protein'),
+        'fat_per_serve': req.get('fat_per_serve'),
+        'carbohydrate': req.get('carbohydrate'),
+        'dietary_fibre': req.get('dietary_fibre'),
+        'cholesterol': req.get('cholesterol'),
+        'energy': req.get('energy')
+        }
+    })
     recipe = req.get('recipe_name')
     flash(f'Recipe {recipe} updated.', 'success')
     return redirect(url_for('show_recipes'))
 
 @app.route('/show_recipes')
 def show_recipes():
-    recipes=mongo.db.recipes.find().collation({'locale':'en'}).sort('recipe_name', 1)
+    recipes = mongo.db.recipes.find().collation({'locale': 'en'}).sort('recipe_name', 1)
     return render_template("show_recipes.html", recipes=recipes, title='Browse recipes')
 
 @app.route('/delete_recipe/<recipe_id>')
@@ -173,12 +186,13 @@ def delete_recipe(recipe_id):
     flash('Recipe deleted.', 'success')
     return redirect(url_for('show_recipes'))
 
-''' The below routes handle administration of cuisine types '''
+''' The routes below handle administration of cuisine types '''
 
 @app.route('/cuisines')
 def cuisines():
     if 'username' in session:
-        return render_template("cuisines.html",title='Cuisines', cuisines=mongo.db.cuisines.find().collation({'locale':'en'}).sort('cuisine_name',1))
+        return render_template("cuisines.html", title='Cuisines',
+                               cuisines=mongo.db.cuisines.find().collation({'locale': 'en'}).sort('cuisine_name',1))
     else:
         flash('You must be logged in to add or edit a cuisine', 'warning')
         return render_template('login.html')
@@ -191,7 +205,7 @@ def edit_cuisine(cuisines_id):
     created_by = created_by
     if user_role == 'admin':
         return render_template('edit_cuisine.html',
-        cuisines=mongo.db.cuisines.find_one({'_id': ObjectId(cuisines_id)}))
+                 cuisines=mongo.db.cuisines.find_one({'_id': ObjectId(cuisines_id)}))
     if created_by != current_user:
         flash('You can only edit cuisines which you have created', 'warning')
         return redirect(url_for('cuisines'))
@@ -228,7 +242,7 @@ def update_cuisine(cuisines_id):
 def delete_cuisine(cuisines_id):
     cuisines=mongo.db.cuisines
     created_by = mongo.db.cuisines.find_one({"_id": ObjectId(cuisines_id)})['created_by']
-    current_user =  session['username']
+    current_user = session['username']
     user_role = mongo.db.users.find_one({"username" : current_user})['role']
     created_by = created_by
     if user_role == 'admin':
@@ -244,7 +258,7 @@ def delete_cuisine(cuisines_id):
         return redirect(url_for('cuisines'))
     return redirect(url_for('cuisines'))
 
-@app.route('/insert_cuisine', methods=['POST','GET'])
+@app.route('/insert_cuisine', methods=['POST', 'GET'])
 def insert_cuisine():
     if request.method == 'POST':
         cuisines = mongo.db.cuisines.find()
